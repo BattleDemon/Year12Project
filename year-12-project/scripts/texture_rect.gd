5bb867d065aa61e897356fa307ddce8163361f1d
DexterH <DexterHart0@outlook.com>
2026-02-16 21:14:36 +1100

    Better Texture

--- year-12-project/scripts/texture_rect.gd
+++ year-12-project/scripts/texture_rect.gd
@@ -2,7 +2,7 @@ extends TextureRect
 
 @onready var image: Image = texture.get_image()
 
-signal selected_color_hex(hex_color)
+signal selectedRegionCode(region_code)
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -14,4 +14,14 @@ func _input(event):
 			hex_color = hex_color.left(6)
 			print(hex_color)
 			
-			selected_color_hex.emit(hex_color)
+			if hex_color == "000000":
+				return
+		
+			var new_hex = "#%s" % hex_color.to_upper()
+			
+			if LoadJsonData.color_region_data.has(new_hex):
+				var region_code = LoadJsonData.color_region_data[new_hex]
+				selectedRegionCode.emit(region_code)
+				print(region_code)
+			else:
+				print("Error: Key does not exist")
